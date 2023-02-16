c466a4731494ec9443b6f8c0e4303951886365b4
stancillous <stancillousray@gmail.com>
2023-02-16 02:39:23 -0800

    hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 
-int main(void){
+/**
+ * main - Entry point
+ *
+ * Description: 'print a string '
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
 
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return 0;
+	return (0);
 
 }
 
