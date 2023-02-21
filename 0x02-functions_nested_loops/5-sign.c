c5ba5bbe777e09bfb195a524190209fb5a21b647
stancillous <stancillousray@gmail.com>
2023-02-21 02:58:59 -0800

    check the sign of an int

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - print the sign of an int
+ * @n: int to be tested
+ * Return: 0 (Success)
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
