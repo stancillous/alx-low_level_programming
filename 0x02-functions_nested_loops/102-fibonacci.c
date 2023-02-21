30023eae09276d44bef911f0ea188c39268f0c20
stancillous <stancillousray@gmail.com>
2023-02-21 05:06:14 -0800

    fibonacci numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - print the fist 100 fibonacci numbers
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int a = 1;
+	int b = 2;
+	int counter = 0;
+	int sum;
+
+	printf("%d, ", a);
+	prinf("%d, ", b);
+
+	while (counter < 98)
+	{
+		sum = a + b;
+		a = b;
+		b = sum;
+
+		printf("%d, ", sum);
+		counter++;
+	}
+	return (0);
+}
