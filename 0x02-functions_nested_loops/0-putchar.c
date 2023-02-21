5d66edb129d4cfb1357530778dcd1d3ccc9c9acd
stancillous <stancillousray@gmail.com>
2023-02-20 21:38:44 -0800

    print a string

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,5 @@
 #include "main.h"
-/*
+/**
  * main - main block
  * Description: print '_putchar'
  * Return: 0 ( Success)
@@ -17,5 +17,5 @@ int main(void)
 	}
 	_putchar('\n');
 
-	Return (0);
+	return (0);
 }
