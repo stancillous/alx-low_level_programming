80fa961477f261375401089bad79e67515c90dfa
stancillous <stancillousray@gmail.com>
2023-02-21 04:52:30 -0800

    101natural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - print the sum of multiples of 3 or 5
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int sum = 0;
+	int num = 0;
+
+	while (num < 1024)
+	{
+		if ((num % 3 == 0) || (num % 5 == 0))
+		{
+			sum += num;
+		}
+		i++;
+	}
+	printf("%d", sum);
+	return (0);
+}
+
