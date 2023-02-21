5ae16de1b0301e8a539f1669e2a9b2b8224ee558
stancillous <stancillousray@gmail.com>
2023-02-21 08:54:48 -0800

    print the fib series

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,36 +1,34 @@
 #include <stdio.h>
-#define LARGEST 10000000000
 /**
  * main - main block
- * Description: Find and print the first 98 fib numbers starting with 1 and 2.
- * Numbers should be coma and space separated.
- * Return: 0
+ * Description: function to print the fib series
+ * Return: 0 (success)
  */
 int main(void)
 {
-	unsigned long int fr1 = 0, bk1 = 1, fr2 = 0, bk2 = 2;
-	unsigned long int hold1, hold2, hold3;
-	int count;
+	unsigned long int res1, res2, res3;
+	unsigned long int fr1 = 0, a = 1, fr2 = 0, b = 2;
+	int counter;
 
-	printf("%lu, %lu, ", bk1, bk2);
-	for (count = 2; count < 98; count++)
+	printf("%lu, %lu, ", a, b);
+	for (counter = 2; counter < 98; counter++)
 	{
-		if (bk1 + bk2 > LARGEST || fr2 > 0 || fr1 > 0)
+		if (a + b > 100000000 || fr2 > 0 || fr1 > 0)
 		{
-			hold1 = (bk1 + bk2) / LARGEST;
-			hold2 = (bk1 + bk2) % LARGEST;
-			hold3 = fr1 + fr2 + hold1;
-			fr1 = fr2, fr2 = hold3;
-			bk1 = bk2, bk2 = hold2;
-			printf("%lu%010lu", fr2, bk2);
+			res1 = (a + b) / 100000000;
+			res2 = (a + b) % 100000000;
+			res3 = fr1 + fr2 + res1;
+			fr1 = fr2, fr2 = res3;
+			a = b, b = res2;
+			printf("%lu%010lu", fr2, b);
 		}
 		else
 		{
-			hold2 = bk1 + bk2;
-			bk1 = bk2, bk2 = hold2;
-			printf("%lu", bk2);
+			res2 = a + b;
+			a = b, b = res2;
+			printf("%lu", b);
 		}
-		if (count != 97)
+		if (counter != 97)
 			printf(", ");
 	}
 	printf("\n");
