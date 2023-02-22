b27609379d615cbb5e95e381ab09fac57496b8b0
stancillous <stancillousray@gmail.com>
2023-02-22 03:44:09 -0800

    test for +ve or -ve integers

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - test for positive or negative ints
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
