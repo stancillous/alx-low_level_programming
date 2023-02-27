0060324109f42b99ea04629f48a6502d2bbae058
stancillous <stancillousray@gmail.com>
2023-02-27 02:36:06 -0800

    print every other character of a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - print even characters of a string
+ * @str: string passed
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (*(str + i) != '\0')
+	{
+		if (*(str + i) % 2 == 0)
+			putchar(*(str + i));
+		i++;
+	}
+}
