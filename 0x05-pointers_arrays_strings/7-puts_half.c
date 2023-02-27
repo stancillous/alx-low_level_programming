6c345dbd9200ad928315b6bac57ddb0c08ed8cff
stancillous <stancillousray@gmail.com>
2023-02-27 12:39:22 -0800

    me

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,7 +8,7 @@
 
 void puts_half(char *str)
 {
-	int i = 0, strlen = 0;
+	int i = 0, strlen = 0, start;
 
 	while (*(str + i) != '\0')
 	{
@@ -16,10 +16,16 @@ void puts_half(char *str)
 		i++;
 	}
 	strlen++;
-	
-	for (strlen /= 2; str[strlen] != '\0'; strlen++)
+
+	if (strlen % 2 == 0)
+		start = strlen / 2;
+	else
+		start = (strlen - 1) / 2;
+
+	while (start < strlen)
 	{
-		_putchar(str[strlen]);
+		_putchar(*(str + start));
+		start++;
 	}
 	_putchar('\n');
 }
