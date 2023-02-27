0e1e9d895446ed32b5867f62174fbca531550dee
stancillous <stancillousray@gmail.com>
2023-02-27 11:40:16 -0800

    rev string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,14 +11,12 @@ void rev_string(char *s)
 	int i, c, l;
 	char h;
 
-	while (s[i] != '\0')
-	{
-		i++;
-	}
+	for (i = 0; s[i] != '\0'; i++)
+		;
 
 	l = i;
 
-	for (i--, c = 0; c < l / 2; i--; c++)
+	for (i--, c = 0; c < l / 2; i--, c++)
 	{
 		h = s[c];
 		s[c] = s[i];
