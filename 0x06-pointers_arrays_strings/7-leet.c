a0b9d31ddb8b9bc6f0a068b98564e5699e779453
stancillous <stancillousray@gmail.com>
2023-03-01 11:39:12 -0800

    leet quiz

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -12,15 +12,15 @@ char *leet(char *s)
 
 	for (j = 0; s[j] != '\0'; j++)
 	{
-		while (s[j] == 'a' || s[j] == 'A')
+		if (s[j] == 'a' || s[j] == 'A')
 			s[j] = '4';
-		while (s[j] == 'e' || s[j] == 'E')
+		else if (s[j] == 'e' || s[j] == 'E')
 			s[j] = '3';
-		while (s[j] == 'o' || s[j] == 'O')
+		else if (s[j] == 'o' || s[j] == 'O')
 			s[j] = '0';
-		while (s[j] == 't' || s[j] == 'T')
+		else if (s[j] == 't' || s[j] == 'T')
 			s[j] = '7';
-		while (s[j] == 'l' || s[j] == 'L')
+		else if (s[j] == 'l' || s[j] == 'L')
 			s[j] = '1';
 	}
 	return (s);
