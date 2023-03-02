08196b311e12150361b28685d32e0414751da941
stancillous <stancillousray@gmail.com>
2023-03-02 04:43:44 -0800

    m

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+/**
+ * main - print a buffer
+ * @b: buffer passed
+ * @size: size of buffer
+ * Return: 0 (Success)
+ */
+
+void print_buffer(char *b, int size)
+{
+    int i, j, k;
+
+    if (size > 0)
+    {
+        for (i = 0; i < size; i += 10)
+        {
+            printf("%.8x:", i);
+            for (j = i; j < i + 10; j++)
+            {
+                if (j % 2 == 0)
+                    printf(" ");
+                if (j < size)
+                    printf("%.2x", b[j]);
+                else
+                    printf("  ");
+            }
+            printf(" ");
+            for (k = i; k < i + 10; k++)
+            {
+                if (k >= size)
+                    break;
+                if (b[k] < 32 || b[k] > 126)
+                    printf("%c", '.');
+                else
+                    printf("%c", b[k]);
+            }
+            printf("\n");
+        }
+
+    }
+
+    else
+    {
+        printf("\n");
+    }
+}
