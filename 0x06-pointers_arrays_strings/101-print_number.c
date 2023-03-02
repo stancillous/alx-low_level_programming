310228facd5562aac2a92a689d374422db61b94d
stancillous <stancillousray@gmail.com>
2023-03-02 02:38:07 -0800

    print integer

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_number - function to print an int
+ * @n: int passed
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	int number, hold, tens = 1;
+
+	/*check if number is +ve or -ve*/
+	if (n < 0)
+	{
+		_putchar('-');
+		number = n * -1; /*make number positive*/
+	}
+	else
+		number = n;
+
+	hold = number;
+
+	while (hold > 9)
+	{
+		tens *= 10; /*count the number of tens every time hold is divisible by 10*/
+		hold /= 10;
+	}
+	for (; tens >= 1; tens /= 10)
+	{
+		/*print the left most number for each loop*/
+		_putchar((number / tens) % 10 + '0');
+	}
+}
