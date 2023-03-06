41f77858d7e42bd133353e28a109bb229d34dad3
stancillous <stancillousray@gmail.com>
2023-03-06 01:25:11 -0800

    locate a char in string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,18 +9,16 @@
 
 char *_strchr(char *s, char c)
 {
-	char *pointer;
 	int i, strlen;
 
 	for (strlen = 0; s[strlen] != '\0'; strlen++)
 		;
 	for (i = 0; i < strlen; i++)
 	{
-		if (s[i] == c)
+		if (*(s + i) == c)
 		{
-			pointer = &s[i];
-			return (pointer);
+			return (s + i);
 		}
 	}
-	return (NULL);
+	return ('\0');
 }
