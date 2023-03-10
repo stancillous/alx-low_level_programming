c47f499d2704faa26016311113cb7b071624b448
stancillous <stancillousray@gmail.com>
2023-03-09 23:02:09 -0800

    print arg count

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - print arg count
+ * @argc: argument count
+ * @argv: array with the args
+ * Return: 0 (success)
+ */
+
 int main(int argc, char __attribute__((__unused__)) *argv[])
 {
 	printf("%d\n", argc - 1);
