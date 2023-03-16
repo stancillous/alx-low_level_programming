1a48719bffe1e055c300ff4cb0f5c94fd57ac1ef
stancillous <stancillousray@gmail.com>
2023-03-16 14:06:21 -0700

    concat strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -32,13 +32,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (num >= len2)
 		num = len2;
 
-	comb = malloc(sizeof(*comb) * (len1 + num) + 1);
+	comb = malloc(sizeof(*comb) * (len1 + num + 1));
 	if (comb == NULL) /*check for null */
 		return (NULL);
 
 	len1 = 0;
 	len2 = 0;
-	
 	/*concat strings*/
 	for (i = 0; s1[len1] != '\0'; i++, len1++)
 		comb[i] = s1[len1];
