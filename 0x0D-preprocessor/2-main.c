3564b885d3da0275df41323f5eaa6dbebba253d6
stancillous <stancillousray@gmail.com>
2023-03-20 02:33:23 -0700

    FILE

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - main function block
+ * Description: prints the name of the file it was compiled from
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	printf(__FILE__);
+	printf("\n");
+	return (0);
+}
