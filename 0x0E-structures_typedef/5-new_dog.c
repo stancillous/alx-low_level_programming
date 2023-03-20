b8c11a991c21e060ce83428e0802d1add3e2b7f8
stancillous <stancillousray@gmail.com>
2023-03-20 06:34:16 -0700

    h

---
+++ 0x0E-structures_typedef/5-new_dog.c
@@ -0,0 +1,23 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - creates a new dog
+ * @name: dog's name
+ * @owner: dog's owner
+ * @age: dog's age
+ * Return: new dog
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *ptr;
+
+	ptr = malloc(sizeof(dog_t));
+	if (ptr == NULL)
+		return (NULL);
+	ptr->name = name;
+	ptr->age = age;
+	ptr->owner = owner;
+	return (ptr);
+}
