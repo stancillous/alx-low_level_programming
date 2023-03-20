d2dfb4116fe2437b22f6c0fccd8e0c56a5a1767b
stancillous <stancillousray@gmail.com>
2023-03-20 05:35:10 -0700

    m

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,10 +1,12 @@
 #include "dog.h"
+#include <stdlib.h>
 
 /**
  * init_dog - init var of struct dog
  * @name: name of dog
  * @owner: owner of the dog
  * @age: age of the dog
+ * @d: pointer
  * Return: void
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,22 +16,5 @@ struct dog
 	char *owner;
 };
 
-/**
- * init_dog - init var of struct dog
- * @name: name of dog
- * @owner: owner of the dog
- * @age: age of the dog
- * Return: void
- */
 void init_dog(struct dog *d, char *name, float age, char *owner);
-
-/**
- * print_dog - prints a struct dog
- * @d: pointer
- * Return: void
- */
-
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
 #endif
