b094bacab376454cbdf5d95c8092cd0ce8ec99e0
stancillous <stancillousray@gmail.com>
2023-03-20 05:03:22 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef POPPY
+#define POPPY
+
+/**
+ *struct dog - dog's attributes
+ * owner: dog owner
+ * @age: dog's age
+ * @name: name of the dog
+ * Description: Define a new type struct dog with the following elements
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
