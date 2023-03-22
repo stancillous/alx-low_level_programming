e9d30c02c4f19856c27773113173fb3075c5f7b9
stancillous <stancillousray@gmail.com>
2023-03-22 10:50:40 -0700

    m

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,50 +1,48 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * print_opcodes - print the opcodes of this program
- * @a: address of the main function
- * @n: number of bytes to print
- *
+ * get_codes - print the opcodes
+ * @a: main func's address
+ * @n: bytes
  * Return: void
  */
-void print_opcodes(char *a, int n)
-{
-  int i;
-  
-  for (i = 0; i < n; i++)
-  {
-    printf("%.2hhx", a[i]);
-    if (i < n - 1)
-      printf(" ");
-    }
-  printf("\n");
 
+void get_codes(char *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%.shhx", a[i]);
+		if (i < n - 1)
+			printf(" ");
+	}
+	printf("\n");
 }
 
 /**
- * main - prints the opcodes of its own main function
- * @argc: argument count
- * @argv: argument vector
- *
- * Return: O
+ * main - main function
+ * @argc: number of args
+ * @argv: array with the args
+ * Return: 0(Success)
  */
+
 int main(int argc, char **argv)
 {
-  int a;
-  
-  if (argc != 2)
-  {
-    printf("Error\n");
-    exit(1);
-  }
-  a = atoi(argv[1]);
-  if (a < 0)
-  {
-    printf("Error\n");
-    exit(2);
-  }
-  print_opcodes((char *)&main, a);
-  return (0);
-}
+	int c;
 
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	c = atoi(argv[1]);
+	if (c < 0)
+	{
+		printf("Error\n");
+		exit(2)
+	}
+	get_codes((char *)&main, a);
+	return (0);
+}
