8d37f6a3037195dfa9da084ad58baf2d4ab8ba9b
stancillous <stancillousray@gmail.com>
2023-03-22 11:34:09 -0700

    operation

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,7 +1,7 @@
 #include "3-calc.h"
 #include <string.h>
 #include <stdlib.h>
-#include <sdio.h>
+#include <stdio.h>
 
 /**
  * get_op_func - get operation passed
@@ -19,7 +19,16 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
-	int i;
+	int i = 0;
+
+	while (i < 5)
+	{
+		if (strcmp(s, ops[i].op) == 0)
+		{
+			return (ops[i].f);
+		}
+		i++;
+	}
 
 	return (0);
 }
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,5 +1,6 @@
-#include "main.h"
+#include "3-calc.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * op_add - addition
