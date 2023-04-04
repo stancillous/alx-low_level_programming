cdc3210e3dd1ee24b53a55216a8ce2ba67505cc3
stancillous <stancillousray@gmail.com>
2023-04-04 13:57:06 -0700

    free list

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+/**
+ * print_listint_safe - prints a linked list
+ * @head: head pointe
+ * Return: number of nodes in list
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+	const listint_t *current = head, *temp = NULL;
+	size_t i = 0;
+
+	if (current == NULL)
+		exit(98);
+
+	while (current != NULL)
+	{
+		temp = current;
+		current = current->next;
+		i++;
+		printf("[%p] %d\n", temp, temp->n);
+
+		if (temp < current)
+		{
+			printf("-> [%p] %d\n", current, current->n);
+			break;
+		}
+	}
+	return (i);
+}
