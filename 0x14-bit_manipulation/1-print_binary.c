dd514c3d7ef0fa0214bd593e710e9ec0a75184b4
stancillous <stancillousray@gmail.com>
2023-04-08 03:13:44 -0700

    print binary rep of anumber

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,28 +8,20 @@
 
 void print_binary(unsigned long int n)
 {
-	int sLen = 0, i = 0;
-	char *str;
+	int i;
 
-	str = malloc(sizeof(*str));
-
-	if (n == 0)
-		str[i] = '0';
-
-	while (n / 2 != 0)
+	for (i = 31; i >= 0; i--)
 	{
-		if (n % 2 == 1)
-			str[i] = '1';
-		if (n % 2  == 0)
-			str[i] = '0';
-		i++;
-		n /= 2;
+		if ((n & (1 << i)) != 0)
+			break;
 	}
-	if (n == 1)
-		str[i] = '1';
-	for (i = 0; str[i] != '\0'; i++)
-		sLen++;
-	for (i = sLen - 1; i >= 0; i--)
-		_putchar(str[i]);
+
+	for (; i >= 0; i--)
+	{
+		if ((n & (1 << i)) != 0)
+			_putchar('1' + 0);
+		else
+			_putchar('0' + 0);
+	}
 	_putchar('\n');
 }
