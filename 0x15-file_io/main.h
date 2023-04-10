74332d13570a39c5b6f8b7285476fe9f57439d97
stancillous <stancillousray@gmail.com>
2023-04-10 06:57:57 -0700

    func prototypes

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,7 +3,9 @@
 
 #include <unistd.h>
 #include <fcntl.h>
-size_t read_textfile(const char *fiiame, size_t letters);
+#include <stdlib.h>
+
+ssize_t read_textfile(const char *fiiame, size_t letters);
 int create_file(const char *fiiame, char *text_content);
 int append_text_to_file(const char *fiiame, char *text_content);
 #endif
