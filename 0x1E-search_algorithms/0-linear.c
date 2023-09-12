b873614b78593af75fcf9c9a940858bad859611c
raymond <stancillousray@gmail.com>
2023-09-12 13:25:33 +0300

    search algorithms

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -10,16 +10,22 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-	size_t i = 0;
+    int array_size = size;
+    int i;
 
-	if (!array)
-		return (-1);
-	while (i < size)
-	{
-		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-		if (array[i] == value)
-			return (i);
-		i++;
-	}
-	return (-1);
+    if (!array)
+    {
+        return (-1);
+    }
+
+    for (i = 0; i < array_size; i++)
+    {
+        printf("Value checked array[%d] = [%d]\n", i, array[i]);
+        if (array[i] == value)
+        {
+            return (i);
+        }
+    }
+    return (-1);
+
 }
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,5 +1,5 @@
-ifndef __SEARCH.H__
-def __SEARCH.H__
+#ifndef __SEARCH.H__
+#define __SEARCH.H__
 
 #include <stdio.h>
 
@@ -7,4 +7,4 @@ int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
 
-endif
+#endif
